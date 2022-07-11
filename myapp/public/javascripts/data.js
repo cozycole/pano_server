2b46fc83088dd4938fc03f61447060b6896a1a94
Cole Tritch <colet@Coles-Mac-mini.local>
2022-07-11 16:28:30 -0700

    adding change scene on button click

--- myapp/public/javascripts/data.js
+++ myapp/public/javascripts/data.js
@@ -2,41 +2,26 @@ var APP_DATA = {
   scenes: [
     {
       id: "0-pano1",
-      name: "1609 E 24th Ave",
-      levels: [
-        {
-          tileSize: 256,
-          size: 256,
-          fallbackOnly: true,
-        },
-        {
-          tileSize: 512,
-          size: 512,
-        },
-        {
-          tileSize: 512,
-          size: 1024,
-        },
-        {
-          tileSize: 512,
-          size: 2048,
-        },
-        {
-          tileSize: 512,
-          size: 4096,
-        },
-      ],
-      faceSize: 4096,
-      initialViewParameters: {
-        pitch: 0,
-        yaw: 0,
-        fov: 1.5707963267948966,
-      },
-      linkHotspots: [],
-      infoHotspots: [],
+      name: "1609 E 24th Avenue 1/5",
+    },
+    {
+      id: "0-pano2",
+      name: "1609 E 24th Avenue 2/5",
+    },
+    {
+      id: "0-pano3",
+      name: "1609 E 24th Avenue 3/5",
+    },
+    {
+      id: "0-pano4",
+      name: "1609 E 24th Avenue 4/5",
+    },
+    {
+      id: "0-pano5",
+      name: "1609 E 24th Avenue 5/5",
     },
   ],
-  name: "Project Title",
+  name: "1609 E 24th Avenue",
   settings: {
     mouseViewMode: "drag",
     autorotateEnabled: false,

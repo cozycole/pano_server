1b33b253457eb63cc27f9ee0cd896b7579fba20c
Cole Tritch <colet@Coles-Mac-mini.local>
2022-07-13 16:29:38 -0700

    deleted all uneeded functions provided by marzipano and added JSON requests for loading new scene sets for each house

--- myapp/public/javascripts/pano.js
+++ myapp/public/javascripts/pano.js
@@ -1,18 +1,3 @@
-/*
- * Copyright 2016 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 "use strict";
 
 (function () {
@@ -22,13 +7,12 @@
   var data = window.APP_DATA;
   var faceSize = 4096;
   var sceneIndex = 0;
+  var scenes;
   var initialViewParameters = {
     pitch: 0,
     yaw: 0,
     fov: 1.5707963267948966,
   };
-  var linkHotspots = [];
-  var infoHotspots = [];
 
   // Grab elements from DOM.
   var panoElement = document.querySelector("#pano");
@@ -42,36 +26,6 @@
   var nextHouseButton = document.querySelector("#nextHouse");
   var prevHouseButton = document.querySelector("#prevHouse");
 
-  // Detect desktop or mobile mode.
-  if (window.matchMedia) {
-    var setMode = function () {
-      if (mql.matches) {
-        document.body.classList.remove("desktop");
-        document.body.classList.add("mobile");
-      } else {
-        document.body.classList.remove("mobile");
-        document.body.classList.add("desktop");
-      }
-    };
-    var mql = matchMedia("(max-width: 500px), (max-height: 500px)");
-    setMode();
-    mql.addListener(setMode);
-  } else {
-    document.body.classList.add("desktop");
-  }
-
-  // Detect whether we are on a touch device.
-  document.body.classList.add("no-touch");
-  window.addEventListener("touchstart", function () {
-    document.body.classList.remove("no-touch");
-    document.body.classList.add("touch");
-  });
-
-  // Use tooltip fallback mode on IE < 11.
-  if (bowser.msie && parseFloat(bowser.version) < 11) {
-    document.body.classList.add("tooltip-fallback");
-  }
-
   // Viewer options.
   var viewerOpts = {
     controls: {
@@ -82,348 +36,7 @@
   // Initialize viewer.
   var viewer = new Marzipano.Viewer(panoElement, viewerOpts);
 
-  // Create scenes.
-  var scenes = data.scenes.map(function (data) {
-    var urlPrefix = "tiles";
-    var source = Marzipano.ImageUrlSource.fromString(
-      urlPrefix + "/" + data.id + "/{z}/{f}/{y}/{x}.jpg",
-      { cubeMapPreviewUrl: urlPrefix + "/" + data.id + "/preview.jpg" }
-    );
-    var geometry = new Marzipano.CubeGeometry(window.APP_DATA.levels);
-
-    var limiter = Marzipano.RectilinearView.limit.traditional(
-      faceSize,
-      (100 * Math.PI) / 180,
-      (120 * Math.PI) / 180
-    );
-    var view = new Marzipano.RectilinearView(initialViewParameters, limiter);
-
-    var scene = viewer.createScene({
-      source: source,
-      geometry: geometry,
-      view: view,
-      pinFirstLevel: true,
-    });
-
-    // Create link hotspots.
-    linkHotspots.forEach(function (hotspot) {
-      var element = createLinkHotspotElement(hotspot);
-      scene
-        .hotspotContainer()
-        .createHotspot(element, { yaw: hotspot.yaw, pitch: hotspot.pitch });
-    });
-
-    // Create info hotspots.
-    infoHotspots.forEach(function (hotspot) {
-      var element = createInfoHotspotElement(hotspot);
-      scene
-        .hotspotContainer()
-        .createHotspot(element, { yaw: hotspot.yaw, pitch: hotspot.pitch });
-    });
-
-    return {
-      data: data,
-      scene: scene,
-      view: view,
-    };
-  });
-
-  // Set up autorotate, if enabled.
-  var autorotate = Marzipano.autorotate({
-    yawSpeed: 0.03,
-    targetPitch: 0,
-    targetFov: Math.PI / 2,
-  });
-  if (data.settings.autorotateEnabled) {
-    autorotateToggleElement.classList.add("enabled");
-  }
-
-  // Set up fullscreen mode, if supported.
-  if (screenfull.enabled && data.settings.fullscreenButton) {
-    document.body.classList.add("fullscreen-enabled");
-    fullscreenToggleElement.addEventListener("click", function () {
-      screenfull.toggle();
-    });
-    screenfull.on("change", function () {
-      if (screenfull.isFullscreen) {
-        fullscreenToggleElement.classList.add("enabled");
-      } else {
-        fullscreenToggleElement.classList.remove("enabled");
-      }
-    });
-  } else {
-    document.body.classList.add("fullscreen-disabled");
-  }
-
-  // Set handler for scene list toggle.
-  sceneListToggleElement.addEventListener("click", toggleSceneList);
-
-  // Start with the scene list open on desktop.
-  if (!document.body.classList.contains("mobile")) {
-    showSceneList();
-  }
-
-  // Set handler for scene switch.
-  scenes.forEach(function (scene) {
-    var el = document.querySelector(
-      '#sceneList .scene[data-id="' + scene.data.id + '"]'
-    );
-    el.addEventListener("click", function () {
-      switchScene(scene);
-      // On mobile, hide scene list after selecting a scene.
-      if (document.body.classList.contains("mobile")) {
-        hideSceneList();
-      }
-    });
-  });
-
-  function sanitize(s) {
-    return s.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;");
-  }
-
-  function switchScene(scene) {
-    scene.view.setParameters(initialViewParameters);
-    scene.scene.switchTo({ transitionDuration: 100 });
-    updateSceneName(scene);
-    updateSceneList(scene);
-  }
-
-  function updateSceneName(scene) {
-    sceneNameElement.innerHTML = sanitize(scene.data.name);
-  }
-
-  function updateSceneList(scene) {
-    for (var i = 0; i < sceneElements.length; i++) {
-      var el = sceneElements[i];
-      if (el.getAttribute("data-id") === scene.data.id) {
-        el.classList.add("current");
-      } else {
-        el.classList.remove("current");
-      }
-    }
-  }
-
-  function showSceneList() {
-    sceneListElement.classList.add("enabled");
-    sceneListToggleElement.classList.add("enabled");
-  }
-
-  function hideSceneList() {
-    sceneListElement.classList.remove("enabled");
-    sceneListToggleElement.classList.remove("enabled");
-  }
-
-  function toggleSceneList() {
-    sceneListElement.classList.toggle("enabled");
-    sceneListToggleElement.classList.toggle("enabled");
-  }
-
-  function createLinkHotspotElement(hotspot) {
-    // Create wrapper element to hold icon and tooltip.
-    var wrapper = document.createElement("div");
-    wrapper.classList.add("hotspot");
-    wrapper.classList.add("link-hotspot");
-
-    // Create image element.
-    var icon = document.createElement("img");
-    icon.src = "img/link.png";
-    icon.classList.add("link-hotspot-icon");
-
-    // Set rotation transform.
-    var transformProperties = [
-      "-ms-transform",
-      "-webkit-transform",
-      "transform",
-    ];
-    for (var i = 0; i < transformProperties.length; i++) {
-      var property = transformProperties[i];
-      icon.style[property] = "rotate(" + hotspot.rotation + "rad)";
-    }
-
-    // Add click event handler.
-    wrapper.addEventListener("click", function () {
-      switchScene(findSceneById(hotspot.target));
-    });
-
-    // Prevent touch and scroll events from reaching the parent element.
-    // This prevents the view control logic from interfering with the hotspot.
-    stopTouchAndScrollEventPropagation(wrapper);
-
-    // Create tooltip element.
-    var tooltip = document.createElement("div");
-    tooltip.classList.add("hotspot-tooltip");
-    tooltip.classList.add("link-hotspot-tooltip");
-    tooltip.innerHTML = findSceneDataById(hotspot.target).name;
-
-    wrapper.appendChild(icon);
-    wrapper.appendChild(tooltip);
-
-    return wrapper;
-  }
-
-  function createInfoHotspotElement(hotspot) {
-    // Create wrapper element to hold icon and tooltip.
-    var wrapper = document.createElement("div");
-    wrapper.classList.add("hotspot");
-    wrapper.classList.add("info-hotspot");
-
-    // Create hotspot/tooltip header.
-    var header = document.createElement("div");
-    header.classList.add("info-hotspot-header");
-
-    // Create image element.
-    var iconWrapper = document.createElement("div");
-    iconWrapper.classList.add("info-hotspot-icon-wrapper");
-    var icon = document.createElement("img");
-    icon.src = "img/info.png";
-    icon.classList.add("info-hotspot-icon");
-    iconWrapper.appendChild(icon);
-
-    // Create title element.
-    var titleWrapper = document.createElement("div");
-    titleWrapper.classList.add("info-hotspot-title-wrapper");
-    var title = document.createElement("div");
-    title.classList.add("info-hotspot-title");
-    title.innerHTML = hotspot.title;
-    titleWrapper.appendChild(title);
-
-    // Create close element.
-    var closeWrapper = document.createElement("div");
-    closeWrapper.classList.add("info-hotspot-close-wrapper");
-    var closeIcon = document.createElement("img");
-    closeIcon.src = "img/close.png";
-    closeIcon.classList.add("info-hotspot-close-icon");
-    closeWrapper.appendChild(closeIcon);
-
-    // Construct header element.
-    header.appendChild(iconWrapper);
-    header.appendChild(titleWrapper);
-    header.appendChild(closeWrapper);
-
-    // Create text element.
-    var text = document.createElement("div");
-    text.classList.add("info-hotspot-text");
-    text.innerHTML = hotspot.text;
-
-    // Place header and text into wrapper element.
-    wrapper.appendChild(header);
-    wrapper.appendChild(text);
-
-    // Create a modal for the hotspot content to appear on mobile mode.
-    var modal = document.createElement("div");
-    modal.innerHTML = wrapper.innerHTML;
-    modal.classList.add("info-hotspot-modal");
-    document.body.appendChild(modal);
-
-    var toggle = function () {
-      wrapper.classList.toggle("visible");
-      modal.classList.toggle("visible");
-    };
-
-    // Show content when hotspot is clicked.
-    wrapper
-      .querySelector(".info-hotspot-header")
-      .addEventListener("click", toggle);
-
-    // Hide content when close icon is clicked.
-    modal
-      .querySelector(".info-hotspot-close-wrapper")
-      .addEventListener("click", toggle);
-
-    // Prevent touch and scroll events from reaching the parent element.
-    // This prevents the view control logic from interfering with the hotspot.
-    stopTouchAndScrollEventPropagation(wrapper);
-
-    return wrapper;
-  }
-
-  // Prevent touch and scroll events from reaching the parent element.
-  function stopTouchAndScrollEventPropagation(element, eventList) {
-    var eventList = [
-      "touchstart",
-      "touchmove",
-      "touchend",
-      "touchcancel",
-      "wheel",
-      "mousewheel",
-    ];
-    for (var i = 0; i < eventList.length; i++) {
-      element.addEventListener(eventList[i], function (event) {
-        event.stopPropagation();
-      });
-    }
-  }
-
-  function findSceneById(id) {
-    for (var i = 0; i < scenes.length; i++) {
-      if (scenes[i].data.id === id) {
-        return scenes[i];
-      }
-    }
-    return null;
-  }
-
-  function findSceneDataById(id) {
-    for (var i = 0; i < data.scenes.length; i++) {
-      if (data.scenes[i].id === id) {
-        return data.scenes[i];
-      }
-    }
-    return null;
-  }
-
-  // #### MY CODE ####
-  function getAddressJson(address) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.responseType = "json";
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        // on receival of new address json (specifies tile paths)
-        // create new req to send prev address json (with distress indicator)
-        // var req = new XMLHttpRequest();
-        // req.open("POST", "addr1.json", true);
-        // req.send();
-        createNewScenes(this.response);
-      }
-    };
-    xhttp.open("GET", "json/addr2.json", true);
-    xhttp.send();
-  }
-  // all scenes will go left to right if I can help it
-  // so that the arrows will line up with the direction
-  // of the changing scenes
-  newSceneLeftButton.style.display = "none";
-
-  newSceneLeftButton.onclick = () => {
-    switchScene(scenes[--sceneIndex]);
-    console.log(`Scene ${sceneIndex + 1}`);
-    if (sceneIndex == 0) {
-      newSceneLeftButton.style.display = "none";
-    }
-    newSceneRightButton.style.display = "inline-block";
-  };
-
-  newSceneRightButton.onclick = () => {
-    switchScene(scenes[++sceneIndex]);
-    console.log(`Scene ${sceneIndex + 1}`);
-    if (sceneIndex == data.scenes.length - 1) {
-      newSceneRightButton.style.display = "none";
-    }
-    newSceneLeftButton.style.display = "inline-block";
-  };
-
-  // Hide prev house button if it is the first in the route
-  if (data.routeIndex == 0) {
-    prevHouseButton.style.display = "none";
-  }
-
-  nextHouseButton.onclick = () => {
-    // request new JSON
-    // create new scenes
-    // reset onclick for both buttons
-    // send old json w property info
-    getAddressJson("addr2.json"); // executes createNewScenes on callback
-  };
+  // Function to create scenes.
   function createNewScenes(addrJson) {
     // mutating global variable here (might be bad idea)
     scenes = addrJson.scenes.map(function (data) {
@@ -456,8 +69,116 @@
         view: view,
       };
     });
-    switchScene(scenes[addrJson.initSceneIndex]);
+    sceneIndex = addrJson.initSceneIndex;
+    switchScene(scenes[sceneIndex]);
   }
+
+  // Set up fullscreen mode, if supported.
+  if (screenfull.enabled && data.settings.fullscreenButton) {
+    document.body.classList.add("fullscreen-enabled");
+    fullscreenToggleElement.addEventListener("click", function () {
+      screenfull.toggle();
+    });
+    screenfull.on("change", function () {
+      if (screenfull.isFullscreen) {
+        fullscreenToggleElement.classList.add("enabled");
+      } else {
+        fullscreenToggleElement.classList.remove("enabled");
+      }
+    });
+  } else {
+    document.body.classList.add("fullscreen-disabled");
+  }
+
+  // Set handler for scene list toggle.
+  sceneListToggleElement.addEventListener("click", toggleSceneList);
+
+  function sanitize(s) {
+    return s.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;");
+  }
+
+  function switchScene(scene) {
+    scene.view.setParameters(initialViewParameters);
+    scene.scene.switchTo({ transitionDuration: 100 }); // 0.1 sec
+    updateSceneName(scene);
+    updateSceneList(scene);
+  }
+
+  function updateSceneName(scene) {
+    sceneNameElement.innerHTML = sanitize(scene.data.name);
+  }
+
+  function updateSceneList(scene) {
+    for (var i = 0; i < sceneElements.length; i++) {
+      var el = sceneElements[i];
+      if (el.getAttribute("data-id") === scene.data.id) {
+        el.classList.add("current");
+      } else {
+        el.classList.remove("current");
+      }
+    }
+  }
+
+  function toggleSceneList() {
+    sceneListElement.classList.toggle("enabled");
+    sceneListToggleElement.classList.toggle("enabled");
+  }
+
+  // #### MY CODE ####
+  function getAddressJson(address) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.responseType = "json";
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        // on receival of new address json (specifies tile paths)
+        // create new req to send prev address json (with distress indicator)
+        // var req = new XMLHttpRequest();
+        // req.open("POST", "addr1.json", true);
+        // req.send();
+        createNewScenes(this.response);
+      }
+    };
+    xhttp.open("GET", address, true);
+    xhttp.send();
+  }
+  // all scenes will go left to right if I can help it
+  // so that the arrows will line up with the direction
+  // of the changing scenes
+  if (sceneIndex == 0) {
+    newSceneLeftButton.style.display = "none";
+  }
+
+  newSceneLeftButton.onclick = () => {
+    switchScene(scenes[--sceneIndex]);
+    console.log(`Scene ${sceneIndex + 1}`);
+    if (sceneIndex == 0) {
+      newSceneLeftButton.style.display = "none";
+    }
+    newSceneRightButton.style.display = "inline-block";
+  };
+
+  newSceneRightButton.onclick = () => {
+    switchScene(scenes[++sceneIndex]);
+    console.log(`Scene ${sceneIndex + 1}`);
+    if (sceneIndex == scenes.length - 1) {
+      newSceneRightButton.style.display = "none";
+    }
+    newSceneLeftButton.style.display = "inline-block";
+  };
+
+  // Hide prev house button if it is the first in the route
+  if (data.routeIndex == 0) {
+    prevHouseButton.style.display = "none";
+  }
+
+  nextHouseButton.onclick = () => {
+    // request new JSON
+    // create new scenes
+    // reset onclick for both buttons
+    // send old json w property info
+    getAddressJson("json/addr2.json"); // executes createNewScenes on callback
+  };
   // Display the initial scene.
-  switchScene(scenes[sceneIndex]);
+  getAddressJson("json/addr1.json");
+  //switchScene(scenes[sceneIndex]);
 })();
